07a709ca72c210e7ae4a97f94aecc7e14a68fe3b
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-03-11 22:41:28 -0700

    worked on server.js

---
+++ models/Article.js
@@ -0,0 +1,32 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+var ArticleSchema = new Schema({
+    // `notes` is an array that stores ObjectIds
+    // The ref property links these ObjectIds to the Note model
+    // This allows us to populate the User with any associated Notes
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    notes: {
+        // Store ObjectIds in the array
+        type: Schema.Types.ObjectId,
+        // The ObjectIds will refer to the ids in the Note model
+        ref: "Note"
+    }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the User model
+module.exports = Article;
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongojs = require("mongojs");
 var mongoose = require("mongoose");
+var logger = require("morgan");
+// var methodOverride = require("method-override");
+
 // Require request and cheerio. This makes the scraping possible
 var request = require("request");
 var cheerio = require("cheerio");
@@ -14,14 +17,15 @@ var app = express();
 var db = require("./models");
 
 var PORT = 3000;
-
+// Use morgan logger for logging requests
+app.use(logger("dev"));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(methodOverride("_method"));
+// app.use(methodOverride("_method"));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -30,16 +34,76 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/scrapedThatNewsdb", {
-    useMongoClient: true
+    // useMongoClient: true
 });
 
+app.get("/scrape", function(req, res) {
+    request("https://www.azcentral.com/", function(error, response, html) {
+        var $ = cheerio.load(html);
+        $("a").each(function(i, element) {
+            var link = $(element).attr("href");
+            var headline = $(element)
+                .children("span.js-asset-headline").text().trim().replace("\n", "");
+            if (headline && link) {
+                db.Article.create({
+                        headline: headline,
+                        link: link
+                    },
+                    function(err, inserted) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log(inserted);
+                        }
+                    }
+                );
+            }
+        });
+    });
+    res.send("Scrape Complete");
+});
 
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("articles/:id", function(req, res) {
+    db.Article.create(req.body)
+        .then(function(dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
 
 // Start the server
 app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+    console.log("App running on port " + PORT + "!");
 });
